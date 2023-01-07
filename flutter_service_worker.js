6ae7745a5b8677ba67c63a7942d4af78fa98b5aa
FallenRiteMonk <fallenritemonk@gmail.com>
2023-01-07 15:08:38 +0000

    Deploying to main from @ mine-buddy/app2@747fa2da91a4701dc25f119985c376577c4740f4 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -21,7 +21,7 @@ const RESOURCES = {
 "splash/img/light-1x.png": "dfbe204d2da23277baf7d734efb00c0a",
 "splash/splash.js": "123c400b58bea74c1305ca3ac966748d",
 "splash/style.css": "e6d7e19be386e363a611d5667912e2b0",
-"main.dart.js": "2d00aff3e3b867dfd37fbbe3279faa5c",
+"main.dart.js": "53e9bb64c61bf3f78279583a14130e8c",
 "canvaskit/canvaskit.wasm": "bf50631470eb967688cca13ee181af62",
 "canvaskit/profiling/canvaskit.wasm": "95a45378b69e77af5ed2bc72b2209b94",
 "canvaskit/profiling/canvaskit.js": "38164e5a72bdad0faa4ce740c9b8e564",
@@ -42,9 +42,9 @@ const RESOURCES = {
 "assets/assets/Monocraft.otf": "c6391aa65c1d0078d1f6417ee84bf469",
 "assets/assets/branding.png": "e4d8ac25371d3b5496d4dac0e5fbd2fa",
 "assets/assets/icon.png": "cfbcde115e7000c2eb9e034b387b296b",
-"assets/NOTICES": "02d136bfceabe23408e404a8270f25f3",
-"index.html": "b8f28b47d4f47aea1a717e6fc1b4d97a",
-"/": "b8f28b47d4f47aea1a717e6fc1b4d97a",
+"assets/NOTICES": "7e230cc7ba62ac1ca1a9a2cbee4c3161",
+"index.html": "f8cab6093c9fe4f44c6fedf1e4da2dc8",
+"/": "f8cab6093c9fe4f44c6fedf1e4da2dc8",
 "favicon.png": "ed84e13599e5c31249e7e76b9a68774b",
 "version.json": "0280cd63e9f242515d46085b7822e700"
 };
