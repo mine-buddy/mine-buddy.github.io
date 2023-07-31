cb5def6b341da20f5eb1703f27b923e1d60aef43
FallenRiteMonk <fallenritemonk@gmail.com>
2023-07-31 19:53:15 +0000

    Deploying to main from @ mine-buddy/app2@e7f83b766a39748099c08d12bccb85da1a6b984d 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"index.html": "836d4c335ea92cebea38939147cfe5a0",
-"/": "836d4c335ea92cebea38939147cfe5a0",
+const RESOURCES = {"index.html": "cbb2db3f6203267fa72e13f267097aa9",
+"/": "cbb2db3f6203267fa72e13f267097aa9",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "c2ee6d0b25737c010807a6fc28defc98",
 "main.dart.js": "fdc0cee9ef89d6fa2920b442d11a7f43",
